d6e0ed0ad1dc9b8eab6ba0c088e3bcebb719cc5c
Neil Bolton <nbolton@webcycle.co.za>
2022-01-17 22:27:05 +0200

    Unit testing.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,52 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'coffee-machine'`, () => {
+  it(`should have as displayMessage 'Select your coffee beverage of choice'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('coffee-machine');
+    expect(app.displayMessage).toEqual('Select your coffee beverage of choice');
   });
 
-  it('should render title', () => {
+  it('should render displayMessage', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('coffee-machine app is running!');
+    expect(compiled.querySelector('.display')?.textContent).toContain('Select your coffee beverage of choice');
+  });
+
+  it(`should have as powerButton equal 'true'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.powerButton).toEqual(true);
+  });
+  
+  it(`should have as currentDate equal todays date`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.currentDate).toEqual(new Date().toDateString());
+  });
+
+  it(`should have as isBrewing equal 'false'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.isBrewing).toEqual(false);
+  });
+  
+  it(`should have as beverageSelected equal ''`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.beverageSelected).toEqual('');
+  });
+  
+  it(`should have as coffeeIngredients equal '{ beans: 25, milk: 20, sugar: 100 }'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.coffeeIngredients).toEqual({ beans: 25, milk: 20, sugar: 100 });
+  });
+  
+  it(`should have as currentOrder equal '{ beans: 0, milk: 0, sugar: 0 }'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.currentOrder).toEqual({ beans: 0, milk: 0, sugar: 0 });
   });
 });
